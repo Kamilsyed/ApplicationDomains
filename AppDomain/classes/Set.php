8994d07a9e703a5d1658c574c02686cc507d5034
mmollica <mmollica@spsu.edu>
2014-04-08 17:26:21 -0400

    Set update with upload function

--- AppDomain/classes/Set.php
+++ AppDomain/classes/Set.php
@@ -55,36 +55,110 @@ class Set
 	//$comment can be null, but should not be for record purposes
 	//$change should be 2 or 3; 2 = Posted || 3 = Rejected
 	//This function can be used to post a rejected, or reject a posted set
-	public function change($fields = array(), $id)
+	public function change($id, $comment, $change)
 	{
+		$user = new User();
+		date_default_timezone_set('America/New_York');
+
+		$fields = array(
+			'type' => $change,
+			'comment' => $comment,
+			'user_changed' => $user->data()->username,
+			'date_added' => date("Y-m-d H:i:s")
+			);
+
 		if(!$this->_db->update('sets', $id, $fields))
 		{
-			throw new Exception('There was a problem updating.');
+			throw new Exception('There was a problem updating: ' . print_r($fields));
 		}
-
 	}
 
 	public function get_last_id()
-	{	
+	{
 
 		$con = mysql_connect("localhost","host","test");
 
-	    if (!$con)
-	        {
-	             die('Could not connect: ' . mysql_error());
-	        }
+		   if (!$con)
+		       {
+		            die('Could not connect: ' . mysqli_error());
+		       }
 
-	    mysql_select_db('test');
-	    $query = "SELECT id FROM sets ORDER BY `date_added` DESC LIMIT 1"; 
-	    $result = mysql_query($query);
-	    $row = mysql_fetch_assoc($result);
-	    $number = $row['id'];
+		   mysql_select_db('test');
+		   $query = "SELECT id FROM sets ORDER BY `date_added` DESC LIMIT 1"; 
+		   $result = mysql_query($query);
+		   $row = mysql_fetch_assoc($result);
+		   $number = $row['id'];
 
-	    return $number;
+		   return $number;
 	}
 
 	public function data()
 	{
 		return $this->_data;
+	}
+	public static function upload($file)
+	{
+		if(Input::exists())
+		{
+        $DB = mysqli_connect('localhost','host','test','test');
+        
+      
+            
+            $fileName = mysqli_real_escape_string($DB, $_FILES[$file]["name"]);
+            $tmpName  = $_FILES[$file]['tmp_name'];       
+            if(!get_magic_quotes_gpc()) 
+            {
+                $fileName = addslashes($fileName);
+            }
+            
+            
+            $fileSize = mysqli_real_escape_string($DB, $_FILES[$file]['size']);
+            $fileData = mysqli_real_escape_string($DB, file_get_contents($_FILES[$file]["tmp_name"]));
+            $fileType = mysqli_real_escape_string($DB, $_FILES[$file]["type"]);
+            $allowedExts = array("gif", "jpeg", "jpg", "png", "pdf"/*, "txt"*/);
+            $temp = explode(".", $_FILES[$file]["name"]);
+            //$ext = end($temp);
+            $temp2 = end($temp);
+            $ext = (string) $temp2;
+            date_default_timezone_set('America/New_York');
+            $time = date("Y-m-d H:i:s");
+            
+            
+
+            echo "<br>";
+            if ((($_FILES[$file]["type"] == "image/gif")
+            || ($_FILES[$file]["type"] == "image/jpeg")
+            || ($_FILES[$file]["type"] == "image/jpg")
+            || ($_FILES[$file]["type"] == "image/pjpeg")
+            //|| ($_FILES["upload"]["type"] == "text/plain")
+            || ($_FILES[$file]["type"] == "image/x-png")
+            || ($_FILES[$file]["type"] == "application/pdf")
+            || ($_FILES[$file]["type"] == "image/png"))
+            && ($_FILES[$file]["size"] < 1024000)
+            && in_array($ext, $allowedExts))
+            {
+            
+
+                try 
+                {
+                    
+                    return array(
+                    	'file_name'=> $fileName,
+                    	'file_type'=> $fileType,
+                    	'file_size'=> $fileSize,
+                    	'file_data'=> $fileData,
+                    	);
+                } 
+                catch (Exception $e) 
+                {
+                    die ('Connection failed: '.$e->getMessage());
+                }
+                
+           
+            }
+            else
+                echo "Not acceptable file.";
+        
+        }
 	}
 }
