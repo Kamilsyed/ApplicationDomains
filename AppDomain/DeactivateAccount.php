178d8f4a368a8d8a584254d48328578ae7f4a5b5
tharrell88 <tharrell@spsu.edu>
2014-04-15 18:00:11 -0400

    Deactivate Account
    Allows users to activate and deactivate account
    TO DO:
    Prevent user from closing accounts with open transactions.

--- AppDomain/DeactivateAccount.php
+++ AppDomain/DeactivateAccount.php
@@ -1,51 +1,17 @@
 <?php
-
 require_once 'core/init.php';
 
 $user = new User();
-
-if(!$user->isLoggedIn())
+if(!$user->hasPermission('admin'))
 {
 	Redirect::to('index.php');
 }
 
-if(Input::exists())
-{
-	if(Token::check(Input::get('token')))
-	{	
-		$validate = new Validate();
-		$validation = $validate->check($_POST, array(
-			'status' => array('required' => true),
-
-			));
-	}
-
-	if($validation->passed())
-	{
-		try
-		{
-			$account->update(array(
-				'status' => Input::get('status'),
-				));
-		}
-		catch(Exception $e)
-		{
-			die($e->getMessage());
-		}
-	}
-	else
-	{
-		foreach($validation->errors() as $error)
-		{
-			echo $error, '<br>';
-		}
-	}
-}
 ?>
 
 <head>
 		<meta charset="utf-8">
-		<title>Deactivate Accounts</title>
+		<title>Account information</title>
 	<link rel="stylesheet" type="text/css" href="bootstrap.css">
     <style>
 	body{
@@ -76,7 +42,7 @@ if(Input::exists())
 	          <div class="navbar-inner">
 	            <div class="container">
 	              <ul class="nav">
-	                <li class="nav-header">Features</li>
+	                <!-- <li class="nav-header">Features</li> -->
 					<li style="margin-left:190px"><a href="adminHomepage.html">Home</a></li>
 	                <li style="margin-left:25px"><a href="aboutus.html" >About Us</a></li>
                     <li  style="margin-left:25px"><a href="services.html">Services</a></li>
@@ -107,26 +73,97 @@ if(Input::exists())
                <form name="form1" method="post" action="" style="margin-left:175px">
 	        	<span id="spryselect1">
 	        	<label for="Account ID:" style="color:#FFF;">Account Name</label>
-	        	<select name="name" id="name">
-	              	  	<?php $con = mysql_connect("localhost","host","test"); 
-						mysql_select_db('test')?> 
-					   	<?php $result = mysql_query('SELECT * FROM accounts'); ?> 
-					   	<?php while($row = mysql_fetch_assoc($result)) { ?> 
-					       	<option value="<?php echo htmlspecialchars($row['name']);?>"> 
-					           	<?php echo htmlspecialchars($row['name']); ?> 
-					       	</option> 
-					   	<?php } ?>
-					   	<?php mysql_close();
-					   	?> 
-           	      </select>
-           	      <input type="checkbox" name="status">
-                <br/>
-	        	<span class="selectRequiredMsg">Please select an item.</span></span>
-	        	<input name="Deactivate" type="submit" value="Deactive" class="btn btn-large btn-success" style="margin-top:-10px;">
-              </form> 
-                
-                
+	        	<select name="account" id="account">
+					<?php
+						$con = mysqli_connect('localhost', 'host', 'test', 'test');
 
+						$query = "SELECT * FROM accounts";
+						$results = mysqli_query($con, $query);
+
+						while($row = mysqli_fetch_assoc($results))
+						{
+							/*SEE ACCOUNT.PHP LINE 123 FOR MORE INFORMATION*/
+							echo "<option value=" . $row['number'] . ">". $row['name'] ."</option>";
+						}
+
+						mysqli_close($con);
+					?>
+           	      </select>
+	        	<span class="selectRequiredMsg">Please select an item.</span></span>
+	        	<input name="Deactivate" type="submit" value="Find" class="btn btn-large btn-success" style="margin-top:-10px;">
+              </form>
+              
+              <?php
+					if(Input::exists())
+					{
+						
+						$acc = new Account();
+						/*SEE ACCOUNT.PHP LINE 123 FOR MORE INFORMATION*/
+						$acc->findByNumber(Input::get('account'));
+						$con = mysqli_connect('localhost', 'host', 'test', 'test');
+						if(!$con){Redirect::to('errors/500.php');}
+						$query = "SELECT * FROM transactions WHERE acct_id='". $acc->data()->number ."'";
+						$res = mysqli_query($con, $query);
+
+
+						if($acc->data()->status == 1)
+						{
+							echo "<form name='disable' method='post' action='disableaccount.php'>";
+							echo "<table id='rounded-corner' summary='Account Information " . $acc->data()->name . "'>";
+							echo "<thead> <h3 style='margin-left:100px; color:#FFFFFF'>Account Information for ". $acc->data()->name ."</h3>";
+							echo "<tr><th scope='col' class='rounded-company'>Acct#</th>";
+							echo "<th scope='col' class='rounded-q3'>Account Name</th>";
+							echo "<th scope='col' class='rounded-q1'>Description</th>";
+							echo "<th scope='col' class='rounded-q3'>User Added</th>";
+							echo "<th scope='col' class='rounded-q3'>Date Added</th>";
+							echo "<th scope='col' class='rounded-q3'>Current Balance</th>";
+							echo "<th scope='col' class='rounded-q1'>Account Status</th>";
+							echo "<th></th>";
+							echo "</tr></thead>";
+							echo "<tbody><tr>";
+							echo "<td>" . $acc->data()->number . "</td>";
+							echo "<td>" . $acc->data()->name . "</td>";
+							echo "<td>" . $acc->data()->description . "</td>";
+							echo "<td>" . $acc->data()->user_added . "</td>";
+							echo "<td>" . $acc->data()->date_added . "</td>";
+							echo "<td>" . $acc->data()->balance. "</td>";
+							echo "<td>Active</td>";
+							echo "<td><button type='submit'>Disable Account</button>";
+							echo "<input name='accounth' type='hidden' value=" . $acc->data()->number . "></input";
+							echo "</tr></tbody></table>";
+							echo "</form>";
+							//echo "<h3 style='margin-left:100px; color:#FFFFFF'>There are no transactions for ". $acc->data()->name ."</h3>";
+						}
+						else
+						{
+							echo "<form name='enable' method='post' action='enableaccount.php'>";
+							echo "<table id='rounded-corner' summary='Account Information " . $acc->data()->name . "'>";
+							echo "<thead> <h3 style='margin-left:100px; color:#FFFFFF'>Account Information for ". $acc->data()->name ."</h3>";
+							echo "<tr><th scope='col' class='rounded-company'>Acct#</th>";
+							echo "<th scope='col' class='rounded-q3'>Account Name</th>";
+							echo "<th scope='col' class='rounded-q1'>Description</th>";
+							echo "<th scope='col' class='rounded-q3'>User Added</th>";
+							echo "<th scope='col' class='rounded-q3'>Date Added</th>";
+							echo "<th scope='col' class='rounded-q3'>Current Balance</th>";
+							echo "<th scope='col' class='rounded-q1'>Account Status</th>";
+							echo "<th></th>";
+							echo "</tr></thead>";
+							echo "<tbody><tr>";
+							echo "<td>" . $acc->data()->number . "</td>";
+							echo "<td>" . $acc->data()->name . "</td>";
+							echo "<td>" . $acc->data()->description . "</td>";
+							echo "<td>" . $acc->data()->user_added . "</td>";
+							echo "<td>" . $acc->data()->date_added . "</td>";
+							echo "<td>" . $acc->data()->balance. "</td>";
+							echo "<td>Inactive</td>";
+							echo "<td><button type='submit'>Activate Account</button>";
+							echo "<input name='accounth' type='hidden' value=" . $acc->data()->number . "></input";
+							echo "</tr></tbody></table>";
+							echo "</form>";
+							//echo "<h3 style='margin-left:100px; color:#FFFFFF'>There are no transactions for ". $acc->data()->name ."</h3>";
+						}
+					}					
+              		?>
 
         	  </div>
           </div>
