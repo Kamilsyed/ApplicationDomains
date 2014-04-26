f71b8495f5b490a16a71661dd318afb461545a4a
tharrell88 <tharrell@spsu.edu>
2014-04-26 13:37:40 -0400

    Disable Account Rework

--- AppDomain/disableaccount.php
+++ AppDomain/disableaccount.php
@@ -1,28 +1,42 @@
 <?php
+
 require_once 'core/init.php';
 
 $user = new User();
 
-//
-	try
-	{
-		Account::disable($_POST['accounth']);
-		$event = new Event();
-		$event->account_status_event($_POST['accountx'], $_POST['accounth'], $_POST['user'], $_POST['act']);
-		Redirect::to('accountdetails.php');
-	}
-	catch(Exception $e)
-	{
-		echo "<?php echo disablefailed(".$_POST['accountx'].") ?>";
-	}
-//
+if(!Input::exists() || $user->data()->groups != 3 || !$user->isLoggedIn())
+{Redirect::('index.php');}
+
+try
+{
+	Account::disable($_POST['accounth']);
+	$event = new Event();
+	$event->account_status_event($_POST['accountx'], $_POST['accounth'], $_POST['user'], $_POST['act']);
+	//Redirect::to('accountdetails.php');
+}
+catch(Exception $e)
+{
+	echo "<form method='post'><input type='hidden' id='failed' name='failed' value='true'></input></form>";
+}
+
 ?>
+
+<body onload="disablefailed()"></body>
 
 <script type="text/javascript">
 
-function disablefailed(account)
+window.onload = function()
 {
-	window.alert("Disabling " + account + " failed, there are currently open transactions for this account.");
+	disablefailed();
+}
+
+function disablefailed()
+{
+	var failed = document.getElementById().value;
+	if(document.getElementById('failed'))
+	{
+		window.alert("Cannot disable this account, open transactions exist!:" + <?php echo $_POST['accountx';] ?>)
+	}
 }
 
 </script>
